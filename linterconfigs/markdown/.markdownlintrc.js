508cbd566b89ebcc86aff6f547a4e07313659899
Trevor Radcliffe <trevor.radcliffe@nextraq.com>
2020-01-15 16:19:54 -0500

    INFRA-19: A few more config changes.

---
+++ linterconfigs/markdown/.markdownlintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "default": false,
+    // Rule customizations for markdownlint go here
+    // https://github.com/DavidAnson/markdownlint/blob/master/doc/Rules.md
+
+    "line_length": {
+        "line_length": 120,
+        "strict": true
+    },
+
+    // ===
+    // Prettier overrides
+    // ===
+    "no-multiple-blanks": false,
+    "list-marker-space": false,
+    "no-inline-html": false
+}
