ce37a77986191a354692fc31a7474a02110dadcc
Trevor Radcliffe <trevor.radcliffe@nextraq.com>
2020-01-17 10:38:20 -0500

    extract vue configs to separate eslint config

--- linterconfigs/javascript/base_eslintrc.js
+++ linterconfigs/javascript/base_eslintrc.js
@@ -4,8 +4,6 @@ module.exports = {
         'sourceType': 'script',
     },
     'extends': [
-        // https://github.com/vuejs/eslint-plugin-vue#bulb-rules
-        'plugin:vue/recommended',
         // https://github.com/gajus/eslint-plugin-jsdoc#eslint-plugin-jsdoc-installation
         'plugin:jsdoc/recommended',
         // https://github.com/standard/standard/blob/master/docs/RULES-en.md
@@ -18,47 +16,7 @@ module.exports = {
         'no-debugger': process.env.PRE_COMMIT ? 'error' : 'off',
         // Only allow `console.log` in development
         'no-console': process.env.PRE_COMMIT ?
-            ['error', { 'allow': ['warn', 'error'] }] :
-            'off',
-        'vue/array-bracket-spacing': 'error',
-        'vue/arrow-spacing': 'error',
-        'vue/block-spacing': 'error',
-        'vue/brace-style': 'error',
-        'vue/camelcase': 'error',
-        'vue/comma-dangle': ['error', 'always-multiline'],
-        'vue/component-name-in-template-casing': ['warning', 'kebab-case'],
-        'vue/dot-location': ['error', 'property'],
-        'vue/eqeqeq': 'error',
-        'vue/key-spacing': 'error',
-        'vue/keyword-spacing': 'error',
-        'vue/no-boolean-default': ['error', 'default-false'],
-        'vue/no-deprecated-scope-attribute': 'error',
-        'vue/no-empty-pattern': 'error',
-        'vue/object-curly-spacing': ['error', 'always'],
-        'vue/space-infix-ops': 'error',
-        'vue/space-unary-ops': 'error',
-        'vue/v-on-function-call': 'error',
-        'vue/v-slot-style': [
-            'error',
-            {
-                'atComponent': 'v-slot',
-                'default': 'v-slot',
-                'named': 'longform',
-            },
-        ],
-        'vue/valid-v-slot': 'error',
-        'jsdoc/check-tag-names': [
-            'warn',
-            {
-                'definedTags': ['vue-prop', 'vue-data', 'vue-computed', 'vue-event'],
-            },
-        ],
-        'jsdoc/no-undefined-types': [
-            'warn',
-            {
-                'definedTypes': ['VueComponent', 'VuexActionContext'],
-            },
-        ],
+            ['error', { 'allow': ['warn', 'error'] }] : 'off',
         'jsdoc/require-description': ['warn', { 'exemptedBy': ['param', 'returns'] }],
         'semi': ['error', 'always'], // This should be default, but doesn't seem to
 
