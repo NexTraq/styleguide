b326d6fde0afa31a19ef025b3a2e67ffc4fe54be
Trevor Radcliffe <trevor.radcliffe@nextraq.com>
2020-01-29 12:10:34 -0500

    INFRA-32: Add function to get changed files

--- linterconfigs/java/style.gradle.kts
+++ linterconfigs/java/style.gradle.kts
@@ -41,3 +41,30 @@ if (gradle.startParameter.taskNames
         enabled = false
     }
 }
+
+fun getChangedFiles(): List<File> {
+    val sourceBranchProp: String? by project
+    val targetBranchProp: String? by project
+    val sourceBranch: String? = if (sourceBranchProp.isNullOrEmpty()) "" else sourceBranchProp
+    val targetBranch: String? = if (targetBranchProp.isNullOrEmpty()) "master" else targetBranchProp
+
+    return "git diff --name-status --diff-filter=dr $targetBranch $sourceBranch"
+            .trim().runCommand().trim().split("\n")
+            .filter { it.isNotEmpty() }
+            .map { filename ->
+                println("Filename: $filename")
+                val scrubbedPath: String = """\w\s+(.+)""".toRegex().find(filename)?.groupValues?.get(1)!!
+                file("$projectDir${File.separator}$scrubbedPath")
+            }.toList()
+}
+
+fun String.runCommand(): String {
+    val proc: Process = ProcessBuilder(*this.split(" ").toTypedArray())
+            .directory(projectDir)
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+
+    proc.waitFor(1, TimeUnit.MINUTES)
+    return proc.inputStream.bufferedReader().readText()
+}
